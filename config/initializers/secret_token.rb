0af2dde27af97b70c56d1ac04e52fae38d49e4b8
Steven Ferguson & Ana Tighe <student@epicodus.com>
2013-10-03 10:02:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EpicodusOverflow::Application.config.secret_key_base = '4a37eaa27fc2f9a381abda34e4fd307d5c4a068e69c74c79a899d8c2551f160dc87cee278c4d25c5f9a2c0fdb1c84c193f2e939d2d03f1a88d8710aaa2715d46'
